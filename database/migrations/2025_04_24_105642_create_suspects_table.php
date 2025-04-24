dcd793f0b81eaaa30e57dcddb2f01111a50c0053
Bahrean <bahreanplusplus@gmail.com>
2025-04-24 11:46:58 +0300

    update file

---
+++ database/migrations/2025_04_24_105642_create_suspects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suspects', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->integer('age')->nullable();
+            $table->string('gender')->default('male');
+            $table->text('description');
+            $table->string('address')->nullable();
+            $table->string('last_known_location');
+            $table->string('status')->default('wanted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suspects');
+    }
+};
