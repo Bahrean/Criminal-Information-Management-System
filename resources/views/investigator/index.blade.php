d68eba21604e6e7d2fc91ab929584dffbd241c1b
Bahrean <bahreanplusplus@gmail.com>
2025-04-24 23:13:54 +0300

    update file

--- resources/views/investigator/index.blade.php
+++ resources/views/investigator/index.blade.php
@@ -11,7 +11,7 @@
 
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
         <div>
-            <h4 class="mb-3 mb-md-0">Welcome to  {{$gender}} <span style="color: rgb(255, 255, 0);font-size: 25px;">{{$profileData->name}}</span></h4>
+            <h4 class="mb-3 mb-md-0">Welcome to  {{$gender}} <span style="color: rgb(255, 255, 0);font-size: 25px;">{{$profileData->name}} Dashboard</span></h4>
         </div>
 
     </div> <!-- row -->
