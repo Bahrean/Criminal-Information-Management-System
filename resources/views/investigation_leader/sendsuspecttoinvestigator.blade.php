1b063b7ac25fd23e22033ba121e7fa3d85dc111f
Bahrean <bahreanplusplus@gmail.com>
2025-04-24 22:45:49 +0300

    update file

--- resources/views/investigation_leader/sendsuspecttoinvestigator.blade.php
+++ resources/views/investigation_leader/sendsuspecttoinvestigator.blade.php
@@ -11,22 +11,22 @@
             <div class="card shadow">
                 <div class="card-body">
                     <h6 class="card-title text-center" style="color: yellow; font-size: 20px;">
-                        <i class="fas fa-users"></i> All Members
+                        <i class="fas fa-users"></i> Please Send Suspect report To Investigator For Further Investigation
                     </h6>
 
                     <div class="table-responsive">
                         <table id="dataTableExample" class="table table-bordered table-striped">
                             <thead>
                                 <tr style="background-color: #003f00;">
-                                    <th style="font-size: 17px; color: white; font-weight: bold;">ID</th>
-                                    <th style="font-size: 17px; color: white; font-weight: bold;">Name</th>
-                                    <th style="font-size: 17px; color: white; font-weight: bold;">Email</th>
+                                <th style="font-size: 17px; color: white; font-weight: bold;">ID</th>
+                                    <th style="font-size: 17px; color: white; font-weight: bold;">Full Name</th>
+                                    <th style="font-size: 17px; color: white; font-weight: bold;">Age</th>
                                     <th style="font-size: 17px; color: white; font-weight: bold;">Gender</th>
-                                    <th style="font-size: 17px; color: white; font-weight: bold;">Photo</th>
-                                    <th style="font-size: 17px; color: white; font-weight: bold;">Report(text)</th>
-                                    <th style="font-size: 17px; color: white; font-weight: bold;">phone</th>
-                                    
+                                    <th style="font-size: 17px; color: white; font-weight: bold;">Description</th>
                                     <th style="font-size: 17px; color: white; font-weight: bold;">Address</th>
+                                    <th style="font-size: 17px; color: white; font-weight: bold;">Last Known Location</th>
+                                    
+                                    <th style="font-size: 17px; color: white; font-weight: bold;">Status</th>
                                     <th style="font-size: 17px; color: white; font-weight: bold;">Send to Investigator</th>
                                 
                                 </tr>
@@ -34,23 +34,19 @@
                             <tbody>
                                 @foreach ($types as $key => $items)
                                     <tr>
-                                        <td>{{ $key + 1 }}</td>
-                                        <td>{{ $items->name }}</td>
+                                    <td>{{ $key + 1 }}</td>
+                                        <td>{{ $items->full_name }}</td>
                                     
-                                        <td>{{ $items->email }}</td>
+                                        <td>{{ $items->age }}</td>
                                         <td>{{ $items->gender }}</td>
-                                        <td>
-                                            <img class="wd-100 rounded-circle" 
-                                                 src="{{ !empty($items->photo) ? url('upload/admin_image/' . $items->photo) : url('upload/no_image.jpg') }}" 
-                                                 alt="profile" 
-                                                 style="width: 50px; height: 50px;">
-                                        </td>
-                                        <td>{{ $items->repotext }}</td>
-
-                                    
-                                
-                                        <td>{{ $items->phone }}</td>
+                                        <td>{{ $items->description }}</td>
                                         <td>{{ $items->address }}</td>
+ 
+                                        <td>{{ $items->last_known_location }}</td>
+
+                                       
+                                
+                                        <td>{{ $items->status }}</td>
                                         <td>      
                                             <a href="{{ route('InvestigatorLeader.sendtoinvestigator', $items->id) }}" 
                                                 class="btn btn-outline-success btn-sm">
